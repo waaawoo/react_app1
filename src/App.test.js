d3e123abe0a481212c2a1b4082f00d0f040f5513
kazuya-minatani <waaawooo37ta2@gmail.com>
2021-03-30 23:17:24 +0900

    scssの導入

--- src/App.test.js
+++ src/App.test.js
@@ -1,5 +1,5 @@
 import { render, screen } from '@testing-library/react';
-import App from './css/App';
+// import App from './css/App';
 
 test('renders learn react link', () => {
   render(<App />);
