c0639bbc34ea351ee298d97773c18b7e3117fee4
kazuya-minatani <waaawooo37ta2@gmail.com>
2021-03-31 09:46:52 +0900

    stateで情報を渡す

--- src/components/app.js
+++ src/components/app.js
@@ -15,19 +15,59 @@ import Greeting from './gretting';
 // }
 
 // props関数呼び出しで使用
-class App_class extends Component{
-  render(){
-    return (
-      // reactは一番上の要素は一つでないといけない
-      <div>
-        {/* 中の要素は複数あっても良い */}
-        <Greeting name = "親の名前を呼び出し1"/>
-        <Greeting name = "親の名前を呼び出し2"/>
-      </div>
+// class App_class extends Component{
+//   render(){
+//     return (
+//       // reactは一番上の要素は一つでないといけない
+//       <div>
+//         {/* 中の要素は複数あっても良い */}
+//         <Greeting name = "親の名前を呼び出し1"/>
+//       </div>
 
-    );
+//     );
+//   }
+// }
+
+// stateでステータスを持たせる記述
+class App_class extends Component{
+  // コンストラクタで初期値を設定する
+  constructor(props){
+    // 親のpropsも読んでおく
+    super(props);
+    // ステートの初期値を設定
+    this.state = {
+      name: 'タル王',
+    };
+  }
+
+  // --------- 使用する関数を作成する -----------
+
+  handleMouseOver(){
+    this.setState({ name: 'Bob' });
+  }
+
+  handleMouseOut(){
+    this.setState({ name: 'Mike' });
+  }
+//  ------------------------------------------
+
+  // 情報を渡す
+  render(){
+    return(
+      <div
+        // mouseoverはjsの関数、そこに無名関数を渡す
+        onMouseOver={() => this.handleMouseOver()}
+        onMouseOut={() => this.handleMouseOut()}
+
+      >
+        {/* this.stateで設定したnameを渡す */}
+        <Greeting name={this.state.name} />
+      </div>
+    )
   }
 }
+
+
 // 外からインポートできるように記載
 // export default App;
 export default App_class;
